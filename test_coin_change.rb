d697a78d684dadef2f6e1d39962082844afe4d11
doverh <doverheld@gmail.com>
2017-02-23 09:11:32 -0500

    1st test fail because undefined method

---
+++ test_coin_change.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "coin_change.rb"
+
+class TestCoinChange < Minitest::Test
+
+	def test_insert_1_return_penny
+		assert_equal({:penny=>1},coin_changer(0.01))
+	end
+
+end
