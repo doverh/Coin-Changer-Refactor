abf7ffc4d484a2e369f4f90df0242426f762d42f
doverh <doverheld@gmail.com>
2017-02-23 09:57:39 -0500

    Test pass for 1 nickel after modification of function

--- coin_change.rb
+++ coin_change.rb
@@ -1,14 +1,19 @@
 
 def coin_changer(amount)
 	change = Hash.new
-	
+
+	if amount >= 0.05
+	   change[:nickel]= amount * 20 #Converts value to integer
+	end
+
+	if amount > 0 and amount < 0.05
+		change[:penny]= amount * 100 #Converts value to integer
+	end
+
 	if amount == 0
 		change
 	end
 
-	if amount > 0 
-		change[:penny]= amount * 100 #Converts value to integer
-	end
 	return change
 end
 
