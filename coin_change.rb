40967f25a07153e9d2eec22f6d558fc1a53e766d
doverh <doverheld@gmail.com>
2017-02-23 13:26:33 -0500

    Adjusting looping

--- coin_change.rb
+++ coin_change.rb
@@ -1,37 +1,49 @@
 
 def coin_changer(amount)
+	coin = { quarter: 0.25, dime: 0.10, nickel: 0.05, penny: 0.01 }	
 	change = Hash.new
 
-	while amount > 0
-
-
-		if amount >0.24
-			change[:quarter] = 1
-			amount-= 0.25
-		end
-
-		if amount >0.19
-			change[:dime] = 2
-			amount-= 0.20
-		end
-		if amount>0.09
-			change[:dime] = 1
-			amount-= 0.10
-		end
-
-		if amount > 0.04 and amount < 0.10 
-			change[:nickel] = 1
-			amount -= 0.05
-		end
-		
-		if amount > 0 and amount <= 0.04
-			change[:penny]= (amount * 100).round #Converts value to integer
-			amount -= amount
-		end
-	end
-
-	return change
+  coin.each do |key, value|
+    change[key] = 0 
+    if amount >= value
+    	amount -= value 
+    	change[key] += 1
+    end
+   end
+  return change
 end
+
+
+# 	while amount > 0
+
+
+# 		if amount >0.24
+# 			change[:quarter] = 1
+# 			amount-= 0.25
+# 		end
+
+# 		if amount >0.19
+# 			change[:dime] = 2
+# 			amount-= 0.20
+# 		end
+# 		if amount>0.09
+# 			change[:dime] = 1
+# 			amount-= 0.10
+# 		end
+
+# 		if amount > 0.04 and amount < 0.10 
+# 			change[:nickel] = 1
+# 			amount -= 0.05
+# 		end
+		
+# 		if amount > 0 and amount <= 0.04
+# 			change[:penny]= (amount * 100).round #Converts value to integer
+# 			amount -= amount
+# 		end
+# 	end
+
+# 	return change
+# end
 
 
 
