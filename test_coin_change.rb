94b5dae515ddfe5f53ffca021172ce310e97c60c
doverh <doverheld@gmail.com>
2017-02-23 09:35:01 -0500

    2 test fail for 2 pennies

--- test_coin_change.rb
+++ test_coin_change.rb
@@ -7,5 +7,9 @@ class TestCoinChange < Minitest::Test
 		assert_equal({:penny=>1},coin_changer(0.01))
 	end
 
+	def test_insert_2_return_penny_2
+		assert_equal({:penny=>2},coin_changer(0.02))
+	end
+
 
 end
